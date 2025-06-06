edd240aece01afdee3d3aa1697ca5034ef43146f
= <=>
2025-06-06 18:27:21 +0200

    sauvegarde avec graphique

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,18 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  
+  private olympics$ = new BehaviorSubject<Olympic[] | null>(null);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    // requete get envoyé 
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
+      
+      // observable = receptacle du get http
+      // get http = requete pour recuperer le contenu du fichier .json
+      // met les données dans le panier olymic$
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
         // TODO: improve error handling
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { ChartData, ChartType } from 'chart.js';
+import { take } from 'rxjs/operators';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Olympic } from 'src/app/core/models/Olympic';
+import { Chart } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+Chart.register(ChartDataLabels); 
 
 @Component({
   selector: 'app-home',
@@ -8,11 +14,63 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public pieChartType: ChartType = 'pie';
+
+  public ChartDataLabels = ChartDataLabels;
+
+  // pour afficher le nom de chaque pays a coté de sa part du graphique
+  public pieChartOptions = {
+  plugins: {
+    datalabels: {
+      formatter: (value: number, context: any) => {
+        return context.chart.data.labels[context.dataIndex];
+      },
+      color: '#000',
+    },
+  },
+};
+
+
+  public nombreDeJO: number = 0;
+  public nombreDePays: number = 0;
+
+  public pieChartData: ChartData<'pie'> = {
+    labels: [],
+    datasets: [{ data: [] }],
+  };
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    console.log('ngOnInit lancé !');
+
+    this.olympicService.loadInitialData().pipe(take(1)).subscribe(() => {
+      
+      this.olympicService.getOlympics().pipe(take(1)).subscribe((olympics) => {
+        
+        if (!olympics || olympics.length === 0) {
+          console.warn('⚠️ Aucune donnée reçue !');
+          return;
+        }
+
+        // Compte le nb total de JO en comptant le nb d'année unique
+        const allYears = olympics
+        .flatMap(country => country.participations.map(p => p.year)); // récupère toutes les années
+        const uniqueYears = [...new Set(allYears)]; // garde uniquement les années uniques
+        this.nombreDeJO = uniqueYears.length;
+
+        this.nombreDePays = olympics.length;
+
+        const labels = olympics.map((country) => country.country);
+        const data = olympics.map((country) =>
+          country.participations.reduce((total, p) => total + p.medalsCount, 0)
+        );
+
+        this.pieChartData = {
+          labels,
+          datasets: [{ data }],
+        };
+      });
+    });
   }
 }
