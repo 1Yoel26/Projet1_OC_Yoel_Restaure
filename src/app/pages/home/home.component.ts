49d93262f4200642355f945630c087a2de54cd0a
= <=>
2025-06-25 14:57:30 +0200

    Graphique responsive et avec donnees en dur fonctionel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,14 @@
   import { Component, OnInit } from '@angular/core';
-  import { ChartData, ChartType } from 'chart.js';
+  import { ChartData, ChartType, plugins } from 'chart.js';
   import { OlympicService } from 'src/app/core/services/olympic.service';
   import { Olympic } from 'src/app/core/models/Olympic';
   import { Chart } from 'chart.js';
   import { Router } from '@angular/router';
+  import ChartDataLabels, { Context } from 'chartjs-plugin-datalabels';
 
+  // enregistrement du plugin pour afficher les nom des pays 
+  // directement sur les parts du graphique
+  Chart.register(ChartDataLabels);
 
   @Component({
     selector: 'app-home',
@@ -17,11 +21,41 @@
     nombreDeJO: number = 0;
     nombreDePays: number = 0;
 
+    // Pour indiquer le type de graphique dans le Html
+    typeGraphique: ChartType = "pie";
+
+    contenuGraphique: ChartData<"pie"> = {
+
+      // noms associés au part du graphique pie
+      labels: ["France", "Italie", "Angleterre"],
+
+      // proportion de chaque part du graphique pour chaque pays
+      datasets: [{data: [20, 45, 35]}]
+    };
+
+
+    // ajout des noms des pays sur les parts du graphique 
+    // en configurant ce plugin :
+
+    // ajout de la propriété CharDataLabel pour le html
+    public ChartDataLabels = ChartDataLabels;
+
+    public pluginDataLabel = {
+      plugins : {
+        datalabels : {
+          formatter : (_value : number, ctx: Context) => {
+            return ctx.chart.data.labels?.[ctx.dataIndex];
+          },
+          color: '#000' // texte avec couleur noir
+        }
+      }
+    };
+
     
+    
+  
 
     ngOnInit(): void {
 
-      
-      
     }
      }
